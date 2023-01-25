767a0ce73be3e8c184fbad7aa88cd17952c9a25b
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-24 20:55:37 -0700

    set loop to change color and close automatically

--- window.rb
+++ window.rb
@@ -1,3 +1,20 @@
 require "ruby2d"
 
+set title: "Hello World!"
+
+# set background: 'blue'
+
+tick = 0
+t = Time.now
+
+update do
+  if tick % 60 == 0
+    set background: "random"
+  end
+  tick += 1
+  if Time.now - t > 5
+    close
+  end
+end
+
 show
