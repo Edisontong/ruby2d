f5b8026c8c177c255bee78767f9e1db60ecc4d36
Edisontong <122298502+Edisontong@users.noreply.github.com>
2023-01-25 14:07:20 -0700

    added code and comments about al possible colors

---
+++ colors.rb
@@ -0,0 +1,8 @@
+require "ruby2d"
+
+s = Square.new(color: "red")
+# A color keyword can be any one of the following: navy, blue, aqua, teal, olive, green, lime, yellow, orange, red, brown, fuchsia, purple, maroon, white, silver, gray, black, and you can also use random
+
+s.color.opacity = 0.8
+
+show
